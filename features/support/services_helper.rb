a9d8e2c34697d083598bd39195c2c577293379c8
Pilar <pilar.romerom07@gmail.com>
2018-05-08 16:17:55 -0500

    order files

--- features/support/services_helper.rb
+++ features/support/services_helper.rb
@@ -5,7 +5,7 @@ module Services_Helper
 
   def request_weather_by_id_city(id_city)
     begin
-      key = "1111111111"
+      key = "b1fb33ebcd6ffc92508e4dfaf0208976"
       service_url = "api.openweathermap.org/data/2.5/forecast?id=#{id_city}&APPID=#{key}"
       get_info = {:url => service_url}
       @current_response = ServiceHelper.send_get(get_info)
